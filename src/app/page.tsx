e3bd81c4fa8366a02ca92fb6e8e0284d9b4d1902
Thomas Dimnet <thomas.dimnet@gmail.com>
2023-06-19 15:22:00 +0200

    Adding branding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
-    <main className="bg-black">
-      <h1 className="text-white">NX</h1>
+    <main className="bg-black h-screen w-screen">
+      <div className="max-w-screen-lg mx-auto pt-5 px-5 h-full">
+        <section className="flex justify-center items-center flex-col h-5/6">
+          <h1 className="text-white text-4xl mb-8 text-center uppercase">
+            Passez au niveau <br />
+            supérieur
+          </h1>
+          <h2 className="text-white text-xl text-center">
+            Améliorez vos Compétences en Ingénierie Informatique
+            <br />
+            avec des Cours Modernes et Complets.
+          </h2>
+        </section>
+      </div>
     </main>
   );
 }
